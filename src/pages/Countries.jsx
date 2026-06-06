26efd748cbfb558c4bb53798f7442b2eb907e1c7
Emirhan Kubilay Demir <emirhan.demir@turktelekom.com.tr>
2026-06-06 10:28:18 +0300

    Improve countries page and country filtering

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,32 +1,34 @@
+import { Link } from "react-router";
 import { outlets } from "../data/outlets";
 
 function Countries() {
 const countries = [...new Set(outlets.map((outlet) => outlet.country))];
 
 return (
-<main className="page-container">
-<section className="content-page">
+<main className="countries-page">
+<section className="countries-content">
 <h1>Countries</h1>
+<p>Browse outlet destinations by country.</p>
 
-<p>
-Browse outlet destinations by country.
-</p>
-
-<div className="countries-grid">
-{countries.map((country) => (
-<div className="country-card" key={country}>
-<h3>{country}</h3>
-
-<p>
-{
-outlets.filter(
+<div className="country-list-grid">
+{countries.map((country) => {
+const outletCount = outlets.filter(
 (outlet) => outlet.country === country
-).length
-}{" "}
-outlets
+).length;
+
+return (
+<Link
+to={`/outlets?country=${country}`}
+className="country-list-card"
+key={country}
+>
+<h3>{country}</h3>
+<p>
+{outletCount} {outletCount === 1 ? "outlet" : "outlets"}
 </p>
-</div>
-))}
+</Link>
+);
+})}
 </div>
 </section>
 </main>
--- src/pages/Outlets.jsx
+++ src/pages/Outlets.jsx
@@ -1,20 +1,37 @@
+import { useSearchParams } from "react-router";
 import { outlets } from "../data/outlets";
 import OutletCard from "../components/OutletCard";
 import { translations } from "../i18n/translations";
 
 function Outlets() {
+const [searchParams] = useSearchParams();
+const selectedCountry = searchParams.get("country");
+
 const language = localStorage.getItem("language") || "en";
 const t = translations[language];
+
+const filteredOutlets = selectedCountry
+? outlets.filter((outlet) => outlet.country === selectedCountry)
+: outlets;
 
 return (
 <main className="page-container">
 <section className="content-page">
-<h1>{t.navOutlets}</h1>
-<p>Browse all outlet destinations in one place.</p>
+<h1>
+{selectedCountry
+? `${selectedCountry} ${t.navOutlets}`
+: t.navOutlets}
+</h1>
+
+<p>
+{selectedCountry
+? `Browse outlet destinations in ${selectedCountry}.`
+: "Browse all outlet destinations in one place."}
+</p>
 </section>
 
 <section className="outlets">
-{outlets.map((outlet) => (
+{filteredOutlets.map((outlet) => (
 <OutletCard
 key={outlet.name}
 outlet={outlet}
